bb20aee3c02c1db31479814900f2a562b7a956f8
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-05-29 13:15:26 +0600

    frontend partially completed

---
+++ resources/views/layout/style.blade.php
@@ -0,0 +1,30 @@
+<!-- responsive tag -->
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- favicon -->
+<!-- <link rel="apple-touch-icon" href="apple-touch-icon.html"> -->
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/frontend/images/fav-icon.png')}}">
+<!-- Bootstrap v4.4.1 css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/bootstrap.min.css')}}">
+<!-- font-awesome css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/font-awesome.min.css')}}">
+<!-- flaticon css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/fonts/flaticon.css')}}">
+<!-- animate css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/animate.css')}}">
+<!-- owl.carousel css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/owl.carousel.css')}}">
+<!-- slick css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/slick.css')}}">
+<!-- off canvas css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/off-canvas.css')}}">
+<!-- magnific popup css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/magnific-popup.css')}}">
+<!-- Main Menu css -->
+<link rel="stylesheet" href="{{asset('assets/frontend/css/rsmenu-main.css')}}">
+<!-- spacing css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/rs-spacing.css')}}">
+<!-- style css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/style.css')}}"> <!-- This stylesheet dynamically changed from style.less -->
+<!-- responsive css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/responsive.css')}}">
